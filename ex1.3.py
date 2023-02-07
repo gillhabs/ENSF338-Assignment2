11663f0dd483d47ae8cb554921a501f4f6ac82f9
chantaeh <chantae.ho@ucalgary.ca>
2023-02-06 20:46:28 -0700

    change cache implementation to use dictionary instead of list

--- ex1.3.py
+++ ex1.3.py
@@ -1,7 +1,6 @@
-def func(n, cache=[1, 1]):
-    if n < len(cache):
+def func(n, cache={0:1, 1:1}):
+    if n in cache:
         return cache[n]
     else:
-        new_val = func(n-1) + func(n-2)
-        cache.append(new_val)
-        return new_val
+        cache[n] = func(n-1) + func(n-2)
+        return cache[n]
